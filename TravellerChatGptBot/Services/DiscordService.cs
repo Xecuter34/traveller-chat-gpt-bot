fff9200a644a0e36219cd6750b0876df25895134
Cory Green <corz13@hotmail.co.uk>
2023-04-19 14:26:08 +0100

    First Implementation

---
+++ TravellerChatGptBot/Services/DiscordService.cs
@@ -0,0 +1,54 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravellerChatGptBot.Services
+{
+    public class DiscordService
+    {
+        private DiscordSocketClient? _client;
+
+        public async Task InitializeBot(string token)
+        {
+            _client = new DiscordSocketClient(new DiscordSocketConfig
+            {
+                MessageCacheSize = 1200,
+                LogLevel = LogSeverity.Debug,
+                AlwaysDownloadUsers = true,
+                GatewayIntents =
+                    GatewayIntents.MessageContent |
+                    GatewayIntents.Guilds |
+                    GatewayIntents.GuildMessages
+            });
+            _client.Log += Log;
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+            
+            _client.MessageUpdated += MessageUpdated;
+            _client.Ready += () =>
+            {
+                Console.WriteLine("Bot is connected!");
+                return Task.CompletedTask;
+            };
+        }
+
+        public DiscordSocketClient GetClient() => _client ?? throw new NullReferenceException(nameof(_client));
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+
+        private async Task MessageUpdated(Cacheable<IMessage, ulong> before, SocketMessage after, ISocketMessageChannel channel)
+        {
+            // If the message was not in the cache, downloading it will result in getting a copy of `after`.
+            var message = await before.GetOrDownloadAsync();
+            Console.WriteLine($"{message} -> {after}");
+        }
+    }
+}
